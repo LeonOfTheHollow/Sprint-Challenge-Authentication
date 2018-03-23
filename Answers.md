fe0e788e45b9e2ae7d254cc08d6011e758044229
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-03-23 12:04:30 -0700

    <Leon> Added written answers
    * Front-end W-I-P

--- Answers.md
+++ Answers.md
@@ -1,5 +1,11 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1.  Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
+* Middleware is 'software glue' - middlewares are applied to other programs in order to extend their functionality or link them to each other.  I use the redux-thunk middleware in the front-end of this app to allow my actions to dispatch functions instead of objects.
+* Sessions are server-side constructs for storing information pertaining to a particular user's actions on the server.
+* bcrypt is a library that provides basic hashing/encryption functionality in JavaScript. This is a critical element of safe authentication.
+* JWTs are parcels of data that are stored on the client-side after being retrieved from a server. By retaining the token locally and using it in server requests, the client's identity is retained.
 1.  What does bcrypt do in order to prevent attacks?
+* bcrypt *hashes* information (passwords/other sensitive info), applying a deterministic algorithm to it to render it in a format that can't be read by humans, and more importantly, cannot be reused in a request, as it has mutated from its original form.  This way, if a malicious agent acquires access to the database, they will still not have the necessary information to recreate the authentication credentials.
 1.  What are the three parts of the JSON Web Token?
+*Header, Payload, and Signature.
---
+++ front-end/killer-jokes/src/actions/index.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+const ROOT_URL = 'http://localhost:5000/api';
+
+export const USER_REGISTERED = 'USER_REGISTERED';
+export const USER_AUTHENTICATED = 'USER_AUTHENTICATED';
+export const USER_UNAUTHENTICATED = 'USER_UNAUTHENTICATED';
+export const AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR';
+export const GET_USERS = 'GET_USERS';
+export const CHECK_IF_AUTHENTICATED = 'CHECK_IF_AUTHENTICATED';
+
+export const authError = error => {
+  return {
+    type: AUTHENTICATION_ERROR,
+    payload: error,
+  };
+};
+
+export const register = function(username, password, confirmPassword, history) {
+  return async (dispatch) => {
+    if (password !== confirmPassword) {
+      dispatch(authError('Password confirmation doesn\'t match!'));
+      return;
+    }
+    console.log("about to try");
+    try {
+      console.log('Began waiting for post.');
+      await axios.post(`${ROOT_URL}/users`, { username, password });
+      dispatch({ type: USER_REGISTERED });
+      history.push('/signin');
+    } catch(err) {
+      console.log('Error posting user:', err);
+      dispatch(authError('Failed to register user'));
+    }
+  };
+};
+
+export const login = function(username, password, history) {
+  return async dispatch => {
+    try {
+      const response = await axios.post(`${ROOT_URL}/login`, { username, password });
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      dispatch({ type: USER_AUTHENTICATED });
+      history.push('/users');
+    } catch(err) {
+      dispatch(authError('There was a problem getting the signin token.'));
+    };
+  };
+};
+
+export const logout = () => {
+  return dispatch => {
+    localStorage.clear('token');
+  };
+};
+
+export const getUsers = function() {
+  return async dispatch => {
+    try {
+      const response = await axios.get(`${ROOT_URL}/users`, {
+        headers: { Authorization: localStorage.getItem('token') },
+      });
+      dispatch({
+        type: GET_USERS,
+        payload: response.data,
+      });
+    } catch(err) {
+      dispatch(authError('failed to fetch users.'));
+    };
+  };
+};
